4127c9e899ef022394b0a37b02abc2efa12ed4c9
manas-011 <manaskumar9262@gmail.com>
2026-01-07 09:32:10 +0530

    Added multiple language support functionality

--- controllers/executor/codeExecutor.controller.go
+++ controllers/executor/codeExecutor.controller.go
@@ -7,6 +7,7 @@ import (
 
 func Execute(ctx context.Context, lang, code, input string) (string, string, error) {
 	switch lang {
+
 	case "go":
 		return RunInDocker(
 			ctx,
@@ -15,7 +16,43 @@ func Execute(ctx context.Context, lang, code, input string) (string, string, err
 			input,
 			[]string{
 				"sh", "-c",
-				"go run /code/main < /code/input.txt",
+				"go run /code/main.go < /code/input.txt",
+			},
+		)
+
+	case "cpp":
+		return RunInDocker(
+			ctx,
+			"gcc:13",
+			code,
+			input,
+			[]string{
+				"sh", "-c",
+				"g++ /code/main.cpp -O2 -o /code/a.out && /code/a.out < /code/input.txt",
+			},
+		)
+
+	case "java":
+		return RunInDocker(
+			ctx,
+			"openjdk:21-slim",
+			code,
+			input,
+			[]string{
+				"sh", "-c",
+				"javac /code/Main.java && java -cp /code Main < /code/input.txt",
+			},
+		)
+
+	case "python":
+		return RunInDocker(
+			ctx,
+			"python:3.12-slim",
+			code,
+			input,
+			[]string{
+				"sh", "-c",
+				"python3 /code/main.py < /code/input.txt",
 			},
 		)
 
